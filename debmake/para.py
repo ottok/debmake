f858e81f457b1bd512cd95146be8afd8419e8266
Osamu Aoki <osamu@debian.org>
2018-08-06 00:00:52 +0900

    Remove -dbg since we use automatic -dbgsym
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- debmake/para.py
+++ debmake/para.py
@@ -146,7 +146,7 @@ Argument may need to be quoted to protect from the shell.
              '--binaryspec',
             action = 'store',
             default = '',
-            help = 'set binary package specs as comma separated list of "binarypackage":"type" pairs, e.g., in full form "foo:bin,foo-doc:doc,libfoo1:lib,libfoo1-dbg:dbg,libfoo-dev:dev" or in short form ",-doc,libfoo1,libfoo1-dbg, libfoo-dev".  Here, "binarypackage" is the binary package name; and optional "type" is chosen from "bin", "data", "dbg", "dev", "doc", "lib", "perl", "python", "python3", "ruby", and "script". If "type" is not specified but obvious, it is set by "binarypackage".  Otherwise it is set to "bin" for the compiled ELF binary.',
+            help = 'set binary package specs as comma separated list of "binarypackage":"type" pairs, e.g., in full form "foo:bin,foo-doc:doc,libfoo1:lib,libfoo-dev:dev" or in short form ",-doc,libfoo1, libfoo-dev".  Here, "binarypackage" is the binary package name; and optional "type" is chosen from "bin", "data", "dev", "doc", "lib", "perl", "python", "python3", "ruby", and "script". If "type" is not specified but obvious, it is set by "binarypackage".  Otherwise it is set to "bin" for the compiled ELF binary.',
             metavar = 'binarypackage[:type]')
     p.add_argument(
             '-e',
