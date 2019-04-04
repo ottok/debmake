f78dd22841344548b10ab726bd577fb0f1ea46e5
Baptiste BEAUPLAT <lyknode@cilg.org>
2019-04-04 16:52:13 +0200

    Update to policy version 4.3.0 - no changes

--- debmake/__init__.py
+++ debmake/__init__.py
@@ -44,7 +44,7 @@ import debmake.untar
 # Basic package information
 #######################################################################
 
-__debian_policy__   = '4.1.4'   # debian policy version
+__debian_policy__   = '4.3.0'   # debian policy version
 __debian_compat__   = '11'       # debian/compat
 __programname__     = 'debmake'
 __version__         = '4.3.1'
