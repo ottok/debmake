6ed685ac978dd7a3c83a4cc73e4daff5dab49777
Osamu Aoki <osamu@debian.org>
2021-06-24 15:05:30 +0900

    Update debian packaging
     * Use merging workflow described in dgit-maint-merge(7)
     * Suport autopkgtest (DEP-8)
     * Add watch
    
    Signed-off-by: Osamu Aoki <osamu@debian.org>

---
+++ debian/tests/testcode1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# check if installed as executable
+LC_ALL=en_US.UTF-8
+export LC_ALL
+debmake --version 2>&1 | grep -q -e 'Copyright © [-0-9 ]* Osamu Aoki <osamu@debian.org>'
+
---
+++ debian/tests/testcode2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+# check if debmake works as expected
+LC_ALL=en_US.UTF-8
+export LC_ALL
+mkdir foo-1.0
+cd foo-1.0
+echo "DUMMY" > dummy
+cd ..
+tar -cvzf foo-1.0.tar.gz foo-1.0
+ln -s foo-1.0.tar.gz foo_1.0.orig.tar.gz
+cd foo-1.0
+debmake
+test -x debian/rules
+
