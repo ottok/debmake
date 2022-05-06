29438f3c871bf66c2f25e0ef9f9ea07fb27d467d
Osamu Aoki <osamu@debian.org>
2022-05-06 16:47:15 +0900

    Fix autopkgtest issues
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- debian/tests/testcode2.sh
+++ debian/tests/testcode2.sh
@@ -2,13 +2,15 @@
 # check if debmake works as expected
 LC_ALL=en_US.UTF-8
 export LC_ALL
+rm -rf foo-1.0
 mkdir foo-1.0
 cd foo-1.0
 echo "DUMMY" > dummy
 cd ..
+rm -f foo-1.0.tar.gz foo_1.0.orig.tar.gz
 tar -cvzf foo-1.0.tar.gz foo-1.0
-ln -s foo-1.0.tar.gz foo_1.0.orig.tar.gz
+ln -sf foo-1.0.tar.gz foo_1.0.orig.tar.gz
 cd foo-1.0
-debmake
+debmake 2>&1
 test -x debian/rules
 
